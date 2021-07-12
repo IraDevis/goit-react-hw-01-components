fe0700deda9803370cb3787dad28f2e95b5db7b2
Iryna Volosozharova <iradevis@gmail.com>
2021-07-12 18:49:36 +0300

    Added eslint settings and modern-normalize

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import Profile from './components/profile/Profile';
-import userData from './components/profile/user.json';
-
-import Statistic from './components/statistic/Statistic';
-import statsData from './components/statistic/statistical-data.json';
-
-import FriendList from './components/friends/FriendList';
-import friendsData from './components/friends/friends.json';
-
-import TransactionHistory from './components/transactions/TransactionHistory';
-import transactionsData from './components/transactions/transactions.json';
+import Profile from "./components/profile/Profile";
+import userData from "./components/profile/user.json";
+import Statistic from "./components/statistic/Statistic";
+import statsData from "./components/statistic/statistical-data.json";
+import FriendList from "./components/friends/FriendList";
+import friendsData from "./components/friends/friends.json";
+import TransactionHistory from "./components/transactions/TransactionHistory";
+import transactionsData from "./components/transactions/transactions.json";
 
 const App = () => {
-    return <div>
-        <h1>Задание 1 - Профиль социальной сети</h1>
-        <Profile
-            name={userData.name}
-            tag={userData.tag}
-            location={userData.location}
-            avatar={userData.avatar}
-            stats={ userData.stats}/>
-        <h1>Задание 2 - Секция статистики</h1>
-        <Statistic title="Upload Stats" stats={statsData}/>
-        <h1>Задание 3 - Список друзей</h1>
-        <FriendList friends={friendsData}/>
-        <h1>Задание 4 - История транзакций</h1>
-        <TransactionHistory items={transactionsData}/>
+  return (
+    <div>
+      <h1>Задание 1 - Профиль социальной сети</h1>
+      <Profile
+        name={userData.name}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      <h1>Задание 2 - Секция статистики</h1>
+      <Statistic title="Upload Stats" stats={statsData} />
+      <h1>Задание 3 - Список друзей</h1>
+      <FriendList friends={friendsData} />
+      <h1>Задание 4 - История транзакций</h1>
+      <TransactionHistory items={transactionsData} />
     </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import styles from './styles.css';
+import "modern-normalize/modern-normalize.css";
+
+import styles from "./styles.css";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
